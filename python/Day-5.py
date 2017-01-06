b67ad789d269204d98c8c1708c03c10885e4ab12
Jai Kathuria <jaikathuria@live.com>
2017-01-06 22:07:24 +0530

    Added Day 5 Problem Solution

---
+++ python/Day-5.py
@@ -0,0 +1,9 @@
+#!/bin/python3
+
+import sys
+N = int(input().strip())
+multiple = 1
+if N >=2 and N <=20:
+    for i in range(1,11):
+        multiple = i*N
+        print("%s x %s = %s" % (N,i,multiple))
