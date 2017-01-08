f0acfc3b196a713d3d5dea40b47e4f62c5e82162
Jai Kathuria <jaikathuria@live.com>
2017-01-08 21:52:51 +0530

    Added Day 6 Problem Solution

---
+++ python/Day-6.py
@@ -0,0 +1,13 @@
+no = int(input())
+for i in range(0,no):
+    mainString = input()
+    k=0
+    evenString = ""
+    oddString = ""
+    for j in mainString:
+        if(k%2):
+            oddString += j
+        else:
+            evenString += j
+        k += 1
+    print(evenString + " " +oddString)
